133fa42c5e42278ce0f70ae9f743b96d93253529
New <chauhanraunaq878@gmail.com>
2019-12-02 23:22:35 +0530

    import components and modules

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Router, navigate} from '@reach/router';
+
+import Navigation from './components/Navigation';
+import Login from './components/Login';
+import Register from './components/Register';
+import Protected from './components/Protected';
+import Content from './components/Content';
 
 function App() {
   return (
