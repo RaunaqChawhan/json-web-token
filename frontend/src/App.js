95ffacdabea2bd40c8aafae58eeda20f3b104a07
New <chauhanraunaq878@gmail.com>
2019-12-06 01:22:21 +0530

    modify change in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,19 +14,26 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   const logOutCallback = async () => {
-
+    await fetch('http://localhost:4000/logout', {
+      method: 'POST',
+      credentials: 'include'
+    })
+    //Clear user from context
+    setUser({});
+    //Navigate back to start page
+    navigate('/');
   }
 
   //First thing get a new access token if a refresh token exist
   useEffect(() => {
     async function checkRefreshToken() {
-      const result = await (await fetch('http://localhost:4000/refresh_token'), {
+      const result = await (await fetch('http://localhost:4000/refresh_token', {
         method: 'POST',
         credentials: 'include', //Needed to include the cookie
         headers: {
           'Content-Type': 'application/json'
         }
-      }).json();
+      })).json();
       setUser({
         accesstoken: result.accesstoken
       });
@@ -35,7 +42,7 @@ function App() {
     checkRefreshToken();
   }, []); //it will fire off this when we mount the application; it will run just one time
 
-  if(loading) return <div>Loading ...</div>
+  if (loading) return <div>Loading ...</div>
 
   return (
     <UserContext.Provider value={[user, setUser]}>
